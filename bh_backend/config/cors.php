cead28d92222d094540ca51ec3cd5c56292a98ef
Nounagnon02 <princekangbode@gmail.com>
2025-10-29 16:22:44 +0100

    finished 1.2

--- bh_backend/config/cors.php
+++ bh_backend/config/cors.php
@@ -5,7 +5,7 @@ return [
 
     'allowed_origins' => [
         env('FRONTEND_URL', 'http://localhost:3000'),
-        'https://legendes-oubliees.vercel.app/',
+        'https://legendes-oubliees.vercel.app',
         'http://localhost:3000',
     ],
     'allowed_origins_patterns' => [],
