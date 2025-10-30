d788a36c64b8b7c48a7720cb87d1dd4b69893030
Nounagnon02 <princekangbode@gmail.com>
2025-10-30 12:49:28 +0100

    finished 1.7

--- start.sh
+++ start.sh
@@ -8,14 +8,14 @@ php artisan migrate --force
 
 # Exécuter les seeders SEULEMENT si c'est le premier déploiement
 # ou si la variable FORCE_SEED est définie
-if [ "$FORCE_SEED" = "true" ] || [ ! -f /var/www/storage/.seeded ]; then
-    echo "Exécution des seeders..."
-    php artisan db:seed --force
+#if [ "$FORCE_SEED" = "true" ] || [ ! -f /var/www/storage/.seeded ]; then
+    #echo "Exécution des seeders..."
+    #php artisan db:seed --force
     # Créer un fichier marqueur pour ne pas réexécuter
-    touch /var/www/storage/.seeded
-else
-    echo "✅ Seeders déjà exécutés, skip..."
-fi
+    #touch /var/www/storage/.seeded
+#else
+    #echo "✅ Seeders déjà exécutés, skip..."
+#fi
 
 # Optimiser l'application
 php artisan config:cache
